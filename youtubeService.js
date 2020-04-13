966042f23f09ed92eac375b844a0c26416ed0a58
Nathaniel <changipro.nathaniel@gmail.com>
2020-04-13 19:25:05 +0930

    the start

---
+++ youtubeService.js
@@ -0,0 +1,129 @@
+const { google } = require('googleapis');
+
+// Put the following at the top of the file
+// right below the'googleapis' import
+const util = require('util');
+const fs = require('fs');
+
+let liveChatId; // Where we'll store the id of our liveChat
+let nextPage; // How we'll keep track of pagination for chat messages
+const intervalTime = 5000; // Miliseconds between requests to check chat messages
+let interval; // variable to store and control the interval that will check messages
+let chatMessages = []; // where we'll store all messages
+
+const writeFilePromise = util.promisify(fs.writeFile);
+const readFilePromise = util.promisify(fs.readFile);
+
+const save = async (path, data) => {
+  await writeFilePromise(path, data);
+  console.log('Successfully Saved');
+};
+
+const read = async path => {
+  const fileContents = await readFilePromise(path);
+  return JSON.parse(fileContents);
+};
+
+const youtube = google.youtube('v3');
+const OAuth2 = google.auth.OAuth2;
+
+const clientId = '590978770865-iq6gcn21a5qeoje4t5hobjoasg82da8p.apps.googleusercontent.com';
+const clientSecret = 'k_5dIpjK4sRnyYoJ4mOXrxVC';
+const redirectURI = 'http://localhost:3000/callback';
+
+// Permissions needed to view and submit live chat comments
+const scope = [
+  'https://www.googleapis.com/auth/youtube.readonly',
+];
+
+const auth = new OAuth2(clientId, clientSecret, redirectURI);
+
+const youtubeService = {};
+
+youtubeService.getCode = response => {
+  const authUrl = auth.generateAuthUrl({
+    access_type: 'offline',
+    scope
+  });
+  response.redirect(authUrl);
+};
+
+// Request access from tokens using code from login
+youtubeService.getTokensWithCode = async code => {
+  const credentials = await auth.getToken(code);
+  youtubeService.authorize(credentials);
+};
+
+// Storing access tokens received from google in auth object
+youtubeService.authorize = ({ tokens }) => {
+  auth.setCredentials(tokens);
+  console.log('Successfully set credentials');
+  console.log('tokens:', tokens);
+  save('./tokens.json', JSON.stringify(tokens));
+};
+
+youtubeService.findActiveChat = async () => {
+  const response = await youtube.liveBroadcasts.list({
+    auth,
+    part: 'snippet',
+    mine: 'true'
+  });
+  const latestChat = response.data.items[0];
+
+  if (latestChat && latestChat.snippet.liveChatId) {
+    liveChatId = latestChat.snippet.liveChatId;
+    console.log("Chat ID Found:", liveChatId);
+  } else {
+    console.log("No Active Chat Found");
+  }
+};
+
+// Update the tokens automatically when they expire
+auth.on('tokens', tokens => {
+  if (tokens.refresh_token) {
+    // store the refresh_token in my database!
+    save('./tokens.json', JSON.stringify(auth.tokens));
+    console.log(tokens.refresh_token);
+  }
+  console.log(tokens.access_token);
+});
+
+// Read tokens from stored file
+const checkTokens = async () => {
+  const tokens = await read('./tokens.json');
+  if (tokens) {
+    auth.setCredentials(tokens);
+    console.log('tokens set');
+  } else {
+    console.log('no tokens set');
+  }
+};
+
+const getChatMessages = async () => {
+  const response = await youtube.liveChatMessages.list({
+    auth,
+    part: 'snippet,authorDetails',
+    liveChatId,
+    pageToken: nextPage
+  });
+  const { data } = response;
+  const newMessages = data.items;
+  chatMessages.push(...newMessages);
+  nextPage = data.nextPageToken;
+  console.log('Total Chat Messages:', chatMessages.length);
+  console.log(data.items[1].snippet.displayMessage, " ", data.items[1].authorDetails.displayName)
+};
+
+youtubeService.startTrackingChat = () => {
+    interval = setInterval(getChatMessages, intervalTime);
+};
+
+youtubeService.stopTrackingChat = () => {
+    clearInterval(interval);
+};
+
+checkTokens();
+
+// As we progress throug this turtorial, Keep the following line at the nery bottom of the file
+// It will allow other files to access to our functions
+module.exports = youtubeService;
